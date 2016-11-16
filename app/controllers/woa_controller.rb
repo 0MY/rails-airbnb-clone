aa9fc1499b3b7c0b60cd56641f927af07087e2cb
Olivier MARTY <marty.olivier@gmail.com>
2016-11-16 15:22:16 +0100

    add renter_id & woa_id validation to booking.rb

--- app/controllers/woa_controller.rb
+++ app/controllers/woa_controller.rb
@@ -1,16 +1,9 @@
 class WoaController < ApplicationController
-    before_action :find_woa, only: [:show]
 
   def show
     @woa = Woa.find(params[:id])
     @booking = Booking.new
   end
-
-# private
-
-#   def woa_params
-#     params.require(:woa).permit(:title, :description, :location, :price, :rent_start_at, :rent_end_at, :owner_id, :size, :hooked_on_wall, !!!TBcontinued!!!)
-#   end
 
 end
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :renter, class_name: 'User'
   belongs_to :woa, class_name: 'Woa'
 
-  # validates :book_start_at, :book_end_at, presence: true
+  # validates :renter_id, :woa_id, :book_start_at, :book_end_at, presence: true
   validates :owner_rating, :renter_rating, inclusion: { in: [0, 1, 2, 3, 4, 5], allow_nil: false }, numericality: { only_integer: true }
 
 end
