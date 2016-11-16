eb6465eb4fd4f68dd86c83ed7e839368268a66b5
Olivier MARTY <marty.olivier@gmail.com>
2016-11-16 14:49:24 +0100

    seed ok

--- app/models/woa.rb
+++ app/models/woa.rb
@@ -1,7 +1,18 @@
 class Woa < ApplicationRecord
-  belongs_to :owner, class_name: 'User'
-  has_many :bookings, foreign_key: 'woa_id'
-
   TYPES = [ "Sculptur", "Painting", "Electronic Art", "Video", "Figurin", "Statue", "Antique","Objet d'Art" ]
   SIZES = [ "S", "M", "L" ]
+
+  belongs_to :owner, class_name: 'User'
+  has_many :bookings, foreign_key: 'woa_id', dependent: :destroy
+
+  # validates :title, :price, :type, :rent_start_at, presence: true
+
+  ## copied from yelp
+  # scope :sorted_by_ratings, -> { all.sort_by { |restaurant| -restaurant.average_rating } }
+
+  # def average_rating
+  #   avg = reviews.reduce(0) { |sum, review| sum += review.rating.to_f / reviews.count }
+  #   avg.round(2)
+  # end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116112622) do
+ActiveRecord::Schema.define(version: 20161116134517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema.define(version: 20161116112622) do
 
   create_table "woas", force: :cascade do |t|
     t.string   "title"
-    t.string   "type"
     t.text     "description"
     t.integer  "price"
     t.integer  "deposit"
@@ -79,6 +78,7 @@ ActiveRecord::Schema.define(version: 20161116112622) do
     t.datetime "updated_at",        null: false
     t.float    "lat"
     t.float    "lng"
+    t.string   "category"
     t.index ["owner_id"], name: "index_woas_on_owner_id", using: :btree
   end
 
