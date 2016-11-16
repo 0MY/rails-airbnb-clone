21d67a349679a27951c3a2bff1dd113baa82c7d5
Olivier MARTY <marty.olivier@gmail.com>
2016-11-16 15:17:28 +0100

    controllers booking#new-create and woa#show done

--- app/controllers/booking_controller.rb
+++ app/controllers/booking_controller.rb
@@ -3,8 +3,28 @@ class BookingController < ApplicationController
   end
 
   def create
+    @woa = Woa.find(params[:cocktail_id])
+    @booking = @woa.bookings.new(booking_params)
+
+    if @booking.save
+      # le flash affichera un bandeau vert et un message de succès.
+      flash[:notice] = "Booking from '#{@booking.book_start_at} to '#{@booking.book_end_at} for Work of Art '#{@woa.title}' has been successfully created"
+      redirect_to @woa
+    else
+      # le flash affichera un bandeau rouge et un message d'erreur.
+      flash[:alert] = "Booking from '#{@booking.book_start_at} to '#{@booking.book_end_at} has not been created for Work of Art '#{@woa.title}'. Check your inputs and try again."
+      # le render fait tourner les validations
+      # et affichera les messages d'erreur correspondants
+      render 'woa/show'
+    end
   end
 
   def show
+  end
+
+private
+
+  def booking_params
+    params.require(:booking).permit(:renter_id, :woa_id, :book_start_at, :book_end_at, :validated_at, :paid_at, :owner_rating, :owner_comment, :renter_rating, :renter_comment)
   end
 end
--- app/controllers/woa_controller.rb
+++ app/controllers/woa_controller.rb
@@ -1,7 +1,16 @@
 class WoaController < ApplicationController
-  def index
-  end
+    before_action :find_woa, only: [:show]
 
   def show
+    @woa = Woa.find(params[:id])
+    @booking = Booking.new
   end
+
+# private
+
+#   def woa_params
+#     params.require(:woa).permit(:title, :description, :location, :price, :rent_start_at, :rent_end_at, :owner_id, :size, :hooked_on_wall, !!!TBcontinued!!!)
+#   end
+
 end
+
