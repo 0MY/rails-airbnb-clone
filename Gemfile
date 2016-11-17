9b54193cfe4c2c289139924a2aa383457fb7fce4
Muriel Berthou Crestey <murielberthoucrestey@gmail.com>
2016-11-17 19:00:26 +0100

    changements Home + gemfile+ajouts Cloudinary+Attachinary, etc.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,11 @@ gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'redis'
 
+gem "cloudinary", "1.1.0"
+gem "attachinary", github: "assembler/attachinary"
+gem "jquery-fileupload-rails"
+gem "coffee-rails"
+
 gem 'sass-rails'
 gem 'jquery-rails'
 gem 'uglifier'
@@ -29,5 +34,3 @@ group :development, :test do
   gem 'listen', '~> 3.0.5'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
-
-
--- app/controllers/woas_controller.rb
+++ app/controllers/woas_controller.rb
@@ -1,22 +1,31 @@
 class WoasController < ApplicationController
-  def index
-
-    if params[:search]
-      if params[:search][:date]
-        @woas = Woa.where(date: params[:search][:date])  #résultats filtrés par le form cherch ou les categories de la page home
-      elsif params[:search][:category]
-        @woas = Woa.where(category: params[:search][:category])
-        # @woas = Woa.where(category: "paint")
-      end
-    else
-      @woas = Woa.all
-    end
-  end
-
-   def show
-    @woa = Woa.find(params[:id])
-    @booking = Booking.new
-  end
+ def index
+      #test de présence du :search ou du :category renvoyé par la page home
+      if params[:search]
+       # filtre par ville et dates compatibles
+       @woas = Woa.where(city: params[:search][:location])
+       @woas.select { | w | can_book?(w, params[:search][:book_start], params[:search][:book_end]) }
+     elsif params[:category]
+       @woas = Woa.where(category: params[:category])
+       # @woas = Woa.where(category: "paint")
+     else
+       @woas = Woa.all
+     end
+ end  def show
+   @woa = Woa.find(params[:id])
+   @booking = Booking.new
+ end  # start_book MUST be < end_book else algo won't work
+ def can_book?(woa, new_start_book, new_end_book)
+   if woa.rent_start_at < new_start_book && new_end_book < woa.rent_end_at
+     book_flag = false
+     woa.bookings.each { | b | b.book_end_at < new_start_book \
+                             && b.book_end_at < new_end_book \
+                             && new_start_book < b.book_start_at \
+                             && new_end_start < b.book_start_at ? book_flag = true : book_flag = false
+                       }
+     book_flag
+   end
+ end
 end
 
 # simpleform for :search url: woas_path method: get do |f|
